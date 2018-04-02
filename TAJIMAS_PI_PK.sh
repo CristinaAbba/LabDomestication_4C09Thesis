bff41aaf0eb0ff640239c0de0d311be0f509f1cd
CristinaAbba <37987879+CristinaAbba@users.noreply.github.com>
2018-04-02 18:55:04 -0400

    Create TAJIMAS_PI_PK.sh

---
+++ TAJIMAS_PI_PK.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Define variables
+
+popoolation=/4/cristina/popoolation_1.2.2
+
+input=/4/cristina/sequence_analysis_paul/pileup_files_PK
+
+output=/4/cristina/sequence_analysis_paul/tajimas_pi_PK
+
+files=(${input}/*.pileup)
+
+#For loop instructions
+
+for file in ${files[@]}
+do
+name=${file}
+base=`basename ${name} .pileup`
+perl ${popoolation}/Variance-sliding.pl \
+	--input ${input}/${base}.pileup \
+	--output ${output}/${base}.pi \
+	--measure pi \
+	--window-size 10000 \
+	--step-size 10000 \
+	--min-count 2 \
+	--min-coverage 4 \
+	--max-coverage 400 \
+	--min-qual 20 \
+	--pool-size 120 \
+	--fastq-type sanger \
+	--snp-output ${output}/${base}.snps \
+	--min-covered-fraction 0.5
+done
