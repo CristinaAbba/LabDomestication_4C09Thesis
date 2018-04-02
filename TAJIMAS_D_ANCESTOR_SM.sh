a546002b0e540c0a90b94d3b0d01103d54c0064c
CristinaAbba <37987879+CristinaAbba@users.noreply.github.com>
2018-04-02 17:41:16 -0400

    Create TAJIMAS_D_ANCESTOR_SM.sh

---
+++ TAJIMAS_D_ANCESTOR_SM.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Define variables
+
+popoolation=/4/cristina/popoolation_1.2.2
+
+input=/4/cristina/sequence_analysis_sarah/pileup_files_SM/ANCESTOR_ALL_aligned_merged.sorted.final.pileup
+
+output=/4/cristina/sequence_analysis_sarah/tajimas_d_SM/ANCESTOR_ALL_aligned_merged.sorted.final.800.D
+
+perl ${popoolation}/Variance-sliding.pl \
+	--input ${input} \
+	--output ${output} \
+	--measure D \
+	--window-size 10000 \
+	--step-size 10000 \
+	--min-count 2 \
+	--min-coverage 4 \
+	--max-coverage 800 \
+	--min-qual 20 \
+	--pool-size 200 \
+	--fastq-type sanger \
+	--min-covered-fraction 0.5
